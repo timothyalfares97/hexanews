88f756890c8086d7d3765c953d001fd1f92217a1
audwinoyong <audwinoyong@live.com>
2018-08-16 11:13:18 +1000

    Add navbar styles

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,17 +1,54 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 
+import { withStyles } from "@material-ui/core/styles";
+
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 
-class Header extends React.Component {
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const styles = {
+  flex: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
+  root: {
+    flexGrow: 1
+  }
+};
+
+interface IProps {
+  styles: any;
+}
+
+class Header extends React.Component<IProps> {
   public render() {
+    const componentStyles = this.props.styles;
+
     return (
-      <div>
+      <div className={componentStyles.root}>
         <AppBar position="static">
           <Toolbar>
-            <Typography variant="title" color="inherit">
+            <IconButton
+              className={componentStyles.menuButton}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="title"
+              color="inherit"
+              className={componentStyles.flex}
+            >
+              News
               <ul>
                 <li>
                   <Link to="/">Hexanews</Link>
@@ -21,6 +58,7 @@ class Header extends React.Component {
                 </li>
               </ul>
             </Typography>
+            <Button color="inherit">Login</Button>
           </Toolbar>
         </AppBar>
       </div>
@@ -28,4 +66,4 @@ class Header extends React.Component {
   }
 }
 
-export default Header;
+export default withStyles(styles)(Header as any);
