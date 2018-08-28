2afbad36729c472b1da75d4056cb4a94e77da76d
timothyalfares <timothy_alfares@yahoo.com>
2018-08-28 20:38:10 +1000

    Add axios library, constant folder, and user repository

---
+++ src/constants/config.ts
@@ -0,0 +1,3 @@
+const API_ENDPOINT = 'http://192.168.0.15:3000/'
+
+export const USER_ENDPOINT = `${API_ENDPOINT}/users`
---
+++ src/domain/repository/UserRepository.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import * as Config from '../../constants/config'
+
+export default class UserRepository {
+
+  createUser = async (email: string, password: string, name: string): Promise<any> => {
+
+    const response = await axios.post(Config.USER_ENDPOINT, {
+      email: email,
+      password: password,
+      name: name,
+    })
+
+    return response
+  }
+}
